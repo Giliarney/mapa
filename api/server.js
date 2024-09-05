abcb86308d2e89075c3c213f2c36a5a7df75fa65
Giliarney Mario <Giliarneym@grupopeterfrut.local>
2024-09-05 15:35:27 -0300

    teste 7

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
-const jsonServer = require('json-server');
+import jsonServer from 'json-server';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const server = jsonServer.create();
-const router = jsonServer.router('server.json');
+const router = jsonServer.router(`${__dirname}/server.json`);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
