8e8c9b6391e1a95a418a71f45a605d4312bdfe63
Giliarney Mario <Giliarneym@grupopeterfrut.local>
2024-09-05 16:17:33 -0300

    correcao

--- src/components/pages/regions/Table.tsx
+++ src/components/pages/regions/Table.tsx
@@ -34,7 +34,7 @@ function TableInfos() {
   const { data: dadosReponse, isLoading } = useQuery<responseDados>({
     queryKey: ["get-dados", currentPage, selectedOrigin],
     queryFn: async () => {
-      const response = await fetch(`https://mapa-69xg.onrender.com/dados?_page=${currentPage}&_per_page=15`);
+      const response = await fetch(`https://api-mapa.vercel.app/dados?_page=${currentPage}&_per_page=15&UF%20Origem=${selectedOrigin}`);
       const data = await response.json();
       return data;
     },
